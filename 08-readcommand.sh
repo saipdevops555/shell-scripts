022c4be9f1ed984b6d2f9299053b4b7335f624e3
saipdevps555 <saipdevops555@gmail.com>
2023-12-14 16:32:57 +0530

    added password  as last line addition

--- 08-readcommand.sh
+++ 08-readcommand.sh
@@ -3,8 +3,9 @@
 
 echo " Please enter username : "
 read -s USERNAME  # value entered above is read and in not visble mode as we used -s 
-echo " Username is :$USERNAME" # This is for printing only , we normally we do not print 
+
 
 echo " Please enter Password: "
 read -s PASSWORD # value entered above is read and in not visble mode as we used -s 
-echo " Username is :$PASSWORD" 
+ 
+echo " Username is :$USERNAME and Password is : $PASSWORD" # # This is for printing only , we normally we do not print 
