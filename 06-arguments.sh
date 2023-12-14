63a6a8daeb5345df3f6e27c9a32171fa1c1e8ab9
saipdevps555 <saipdevops555@gmail.com>
2023-12-14 16:02:36 +0530

    adding arguments for the variables for runtime

---
+++ 06-arguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1 # passing as runtime args 
+PERSON2=$2 # -passing as runtime args 
+
+echo " $PERSON1 : How are you $PERSON2"
+echo " $PERSON2 : Am good $PERSON1 ? How are you "
+echo " $PERSON1 : Am good too, how are things at ur end $PERSON2"
