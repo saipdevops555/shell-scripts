c57117b23d30f9774f6111e39152495b76e84b2b
saipdevps555 <saipdevops555@gmail.com>
2023-12-14 16:29:11 +0530

    added the password code as well

--- 08-readcommand.sh
+++ 08-readcommand.sh
@@ -3,5 +3,8 @@
 
 echo " Please enter username : "
 read -s USERNAME  # value entered above is read and in not visble mode as we used -s 
- echo " Username is :$USERNAME" # This is for printing only , we normally we do not print 
- 
+echo " Username is :$USERNAME" # This is for printing only , we normally we do not print 
+
+echo " Please enter Password: "
+read -s PASSWORD # value entered above is read and in not visble mode as we used -s 
+echo " Username is :$PASSWORD" 
