8844d449aced565e12bba5ce741a654807d6c020
saipdevps555 <saipdevops555@gmail.com>
2023-12-14 15:36:19 +0530

    changed one line in the code

--- 04-variables2.sh
+++ 04-variables2.sh
@@ -5,4 +5,4 @@ PERSON2=Suresh
 
 echo " $PERSON1 : How are you $PERSON2"
 echo " $PERSON2 : Am good $PERSON1 ? How are you "
-echo " $PERSON1 : Am good too, how are things at ur end "
+echo " $PERSON1 : Am good too, how are things at ur end $PERSON2"
