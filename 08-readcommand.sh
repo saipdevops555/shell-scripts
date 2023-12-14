ce8e885c95d1fb7e0af617fbdf1b49901ad00572
saipdevps555 <saipdevops555@gmail.com>
2023-12-14 16:25:35 +0530

    read variable for username

---
+++ 08-readcommand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo " Please enter username : "
+read -s USERNAME  # value entered above is read and in not visble mode as we used -s 
+ echo " Username is :$USERNAME" # This is for printing only , we normally we do not print 
+ 
