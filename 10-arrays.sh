5f23c781fdccdccc9af6b8f9774afb86bad450bf
saipdevps555 <saipdevops555@gmail.com>
2023-12-14 17:15:54 +0530

    Array concept today

--- 10-arrays.sh
+++ 10-arrays.sh
@@ -2,7 +2,7 @@
 
 FRUITS={"Apple""Banana""Orange"}
 
-echo " First Fruit is : ${FRUITS[0]}"
-echo " Second Fruit is : ${FRUITS[1]}"
-echo " Third Fruit is : ${FRUITS[2]}"
-echo " All Fruits is : ${FRUITS[@]}"
+echo "First Fruit is:${FRUITS[0]}"
+echo "Second Fruit is:${FRUITS[1]}"
+echo "Third Fruit is:${FRUITS[2]}"
+echo "All Fruits is: ${FRUITS[@]}"
